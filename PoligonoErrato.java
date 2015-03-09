a19c42ddef4c461cc4b47ff6c86f7cd608958a54
root <root@localhost.localdomain>
2015-03-09 14:45:16 -0400

    fixes

--- PoligonoErrato.java
+++
@@ -1,7 +0,0 @@
-package treD ;
-
-public class PoligonoErrato extends Exception {
-      public PoligonoErrato (String messaggio) {
-        super (messaggio) ;
-      }
-}
--- Posizionabile.java
+++
@@ -1,7 +0,0 @@
-package treD ;
-
-public interface Posizionabile {
-      public void setPosizione ( Posizione pos)  ;
-      public Posizione getPosizione () ;
-      public void ruota (float angolo ) ;
-}
